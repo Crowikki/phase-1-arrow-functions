37ab5caebd108ff400019f9fafd37debd74c01db
+ <+>
2023-11-24 16:42:24 -0600

    KT Arrow Functions

---
+++ index.js
@@ -0,0 +1,9 @@
+//(parameter1, parameter2) => parameter1 + parameter2;
+// Parameter list ^^^^^   // Function Body ^^^^^^^^
+const divide = (a,b) => a / b;
+
+const square = (a) => a * a;
+
+const add = (a , b) => a + b;
+
+// remeber they are parameters you are entering in the functions
